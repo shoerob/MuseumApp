aa6491f843b17ee0078075bfda0f4a3045f5cbe2
Robert Shoemate <shoemate@telerik.com>
2013-11-08 11:04:42 -0600

    Changes made by Robert Shoemate (shoemate@telerik.com).

--- MuseumApp/scripts/app/main.js
+++ MuseumApp/scripts/app/main.js
@@ -713,8 +713,8 @@ var app = (function () {
                         artifact.Tag = $newTag.val();
                         artifact.UserId = usersModel.currentUser.get('data').Id;
                         artifact.ExihibitId = $exhibitId;
-                    	//alert(JSON.stringify(r.response.Result[0].Id));
-                    	artifact.Image = r.response.Result[0].Id;  // THIS NO WORKIE!!!!! WHaaaaaaa!
+                    	//alert(JSON.stringify(r.response));
+                    	//artifact.Image = r.response.Result[0].Id;  // THIS NO WORKIE!!!!! WHaaaaaaa!
                         artifacts.one('sync', function () {
                             mobileApp.navigate('#:back');
                         });
